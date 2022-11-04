7008d8ba8f1c2c04c436402a7b1d53ec097b4fc4
Nigel Dias <nigeldias2012@gmail.com>
2022-11-04 09:16:15 -0300

    updating scripts

--- scripts/run_rocker.sh
+++ scripts/run_rocker.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+DATA_PATH=$1
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 rocker  --x11 \
         --devices /dev/dri \
         --name kltvo \
-        --oyr-run-arg " -v /media/nigel/copel/:/data/ -v $SCRIPT_DIR/../:/root/kltvo/"  \
+        --oyr-run-arg " -v $DATA_PATH:/data/ -v $SCRIPT_DIR/../:/root/kltvo/"  \
         kltvo:latest bash
