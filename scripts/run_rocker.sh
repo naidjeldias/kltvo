453957c7af32601045b9804377bbe11317c7ec49
Nigel Dias <nigeldias2012@gmail.com>
2022-11-04 09:01:22 -0300

    script to run rocker with intel integrated graphics

--- scripts/run_rocker.sh
+++ scripts/run_rocker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-rocker  --nvidia --x11 \
+rocker  --x11 \
+        --devices /dev/dri \
         --name kltvo \
         --oyr-run-arg " -v /media/nigel/copel/:/data/ -v $SCRIPT_DIR/../:/root/kltvo/"  \
         kltvo:latest bash
