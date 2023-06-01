78bc7999c0685cb2a27ac893569d9a02783d9da8
Nigel Dias <nigeldias2012@gmail.com>
2023-06-01 10:14:19 -0300

    fix(scripts/runKITTIbenchmark.sh): configuring script with new runtime parametrization

--- scripts/runKITTIbenchmark.sh
+++ scripts/runKITTIbenchmark.sh
@@ -1,31 +1,19 @@
 #!/bin/bash
-seqs=(11 12 13 14 15 16 17 18 19 20 21)
-TIME=5
+DATA_PATH=$1
+# seqs=(11 12 13 14 15 16 17 18 19 20 21)
+seqs=(00 01 02 03 04 05 06 07 08 09 10)
+TIME=3
 
 echo "-------------------"
-echo "Preparing to run the algorithm on KITTI benchmark sequences ..."
-
-echo "Creating results folder ..."
-cd results/
-mkdir KITTI
-cd ..
-
-echo "Creating stats folder ..."
-cd stats
-mkdir KITTI
-cd ..
-
+echo "Preparing to run the algorithm on KITTI sequences ..."
 
 for SEQ in ${seqs[@]}; do
     echo "-------------------"
     echo "Runing sequence number $SEQ"
     echo "Wait for $TIME seconds ..."
-    RESULT_PATH="results/KITTI/"
-    STATS_PATH="stats/KITTI/"
-    FILE_NAME="$SEQ"
     sleep $TIME
     STARTTIME=$(date +%s)
-      ./stereo_kitti $SEQ $RESULT_PATH $FILE_NAME $STATS_PATH
+      ./stereo_kitti $DATA_PATH/$SEQ $SEQ
     ENDTIME=$(date +%s)
     echo "It takes $[$ENDTIME - $STARTTIME] seconds to complete this task..."
 done
